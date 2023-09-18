333c234977aa1c82063f563bdd36ca5ce741a2c3
vadempur <vadempur@gmail.com>
2023-09-18 14:26:40 +0530

    contract file

---
+++ contracts/Mood.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract MoodDailry{
+
+    string mood;
+
+    function setMood(string memory _mood) public {
+        mood = _mood;
+    }
+
+    function getMood() public view returns (string memory) {
+        return mood;
+    }
+
+}
